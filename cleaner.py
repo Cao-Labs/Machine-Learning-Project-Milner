dcfeff946fe3530f9e0bb9bf349c4ac8140b6432
Leet Unova <leetunova@gmail.com>
2025-11-25 01:48:48 -0800

    Started workingon the cleaner again.

--- cleaner.py
+++ cleaner.py
@@ -1,5 +1,14 @@
 # This script will take a folder of files as input and output a combined cleaned csv file.
 
+import sys, os
+
+def dict_to_str(value: dict) -> str:
+    return ";".join([f'{k}:{v}' for k, v in value.items()])
+
+def read_file(filename: str) -> str:
+    with open(filename) as file:
+        return file.read()
+
 class DataPoint:
     def __init__(self) -> None:
         self.group_id: str = None
@@ -10,12 +19,44 @@ class DataPoint:
         self.stimuli_presented: str = None
         self.trial_template: str = None
         self.trial_duration: int = None
-        self.next = None
+        self.part_blocks: int = None
+        self.all_blocks: int = None
+        self.pattern: str = None
+        self.trial_num: int = None
+        self.pb: str = None
+        self.final: str = None
+        self.part: int = None
+        self.correct: int = None
+        self.invRT: float = None
+        self.trial: int = None
+        self.group: str = None
 
-def readFile(filename: str) -> str:
-    pass
+    def from_str(text: str) -> DataPoint:
+        print(text)
+        quit()
 
-def getData(text: str) -> DataPoint:
-    pass
+    def __repr__(self) -> str:
+        return dict_to_str(self.__dict__)
 
+    def create_dataset(path: str) -> list[DataPoint]:
+        dataset: list[DataPoint] = []
 
+        if os.path.isdir(path):
+            for member in os.listdir(path):
+                dataset.append(DataPoint.create_dataset(f"{path}/{member}"))
+
+            return dataset
+
+        text: str = read_file(path)
+
+        for line in text.split("\n"):
+            data_point: DataPoint = DataPoint.from_str(line)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        quit()
+
+    dataset: list[DataPoint] = []
+
+    for arg in sys.argv[1:]:
+        dataset += DataPoint.create_dataset(arg)
