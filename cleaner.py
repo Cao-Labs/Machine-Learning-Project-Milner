6ceef3dc9617f0b22e540d1eecbcab3ca3983a90
Leet Unova <leetunova@gmail.com>
2025-11-25 14:32:04 -0800

    Worked on output of cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -26,8 +26,8 @@ class DataPoint:
             key: str = None,
             pressed_or_released: str = None,
             correct_response: str = None,
-            reaction_time: str = None,
             error_code: str = None,
+            reaction_time: str = None,
             p_or_b: str = None) -> None:
         self.participant_group: str = participant_group
         self.participant_name: str = participant_name
@@ -41,13 +41,33 @@ class DataPoint:
         self.pressed_or_released: str = pressed_or_released
         self.correct_response: str = correct_response
         self.error_code: str = error_code
-        self.reaction_time: str = reaction_time
+        self.reaction_time: int = int(reaction_time)
         self.p_or_b: str = p_or_b
 
     def from_str(text: str) -> DataPoint:
         splits: list[str] = text.split("\t")
 
         if len(splits) < 14:
+            return None
+
+        block_name: str = splits[3].strip().lower()
+
+        if block_name == "instructions":
+            return None
+
+        if block_name == "practice":
+            return None
+
+        if block_name == "debreifing":
+            return None
+
+        if block_name == "practice_instructions":
+            return None
+        
+        if block_name == "inverse_practice_instructions":
+            return None
+
+        if block_name == "inverse_instructions":
             return None
 
         return DataPoint(*splits)
@@ -73,10 +93,31 @@ class DataPoint:
 
         return dataset
 
+    def generate_point(self) -> str:
+        return ",".join([
+            "",
+            "DispreferenceFirst",
+            {
+                "disfirst_reg": "dispreferenceFirst",
+                "disfirst_inverse": "dispreferenceFirst_inverse"
+            }[self.participant_group.lower()],
+            str(-1000/self.reaction_time),
+            "",
+            "",
+            self.correct_response
+        ])
+
     def generate_csv(dataset: list[DataPoint]) -> str:
         lines: list[str] = [
-            "Participant Group,Participant Name,Session ID,Block Name,Trial Name,Event Name,Cumulative Time,Participant Response,Key,Pressed Or Released,Correct Response,Error Code,Reaction Time,P or B"
+            ",expt_id,Group,group_id,invRT,Trial,participant_id,response_correct,response_name,response_rt,stimuli_presented,trial_template,trial_duration,PartBlocks,AllBlocks,Pattern,trial_num,PB,Final,Part,Correct"
         ]
+
+        i: int = 1
+        for data_point in dataset:
+            lines.append(f"{i}," + data_point.generate_point())
+            i += 1
+
+        return "\n".join(lines)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -87,5 +128,4 @@ if __name__ == "__main__":
     for arg in sys.argv[1:]:
         dataset += DataPoint.create_dataset(arg)
 
-    print(len(dataset))
-    print(dataset[0])
+    write_file("lclean.csv", DataPoint.generate_csv(dataset))
