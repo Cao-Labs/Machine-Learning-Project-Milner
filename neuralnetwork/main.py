ea9e2f3cbf02b9c410a0a19919706b0ba6000dbf
Leet Unova <leetunova@gmail.com>
2025-11-30 06:43:57 -0800

    Used sklearn to create a neural network to predict response time based off of whether the trial was voiced and whether a vowel occurred after or not.

---
+++ neuralnetwork/main.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import accuracy_score
+
+def read_file(filename: str) -> str:
+    with open(filename) as file:
+        return file.read()
+
+def load_data(text: str) -> tuple[list[list[float]], list[float]]:
+    data: list[list[float]] = []
+    labels: list[float] = []
+    lines: list[str] = text.split('\n')
+
+    for line in lines[1:]:
+        trial: list[str] = line.split(',')
+        try:
+            data.append([1 if trial[-4] == "b" else 0, 1 if trial[-3] == "Final" else 0])
+            labels.append(float(trial[9]))
+        except:
+            print(f"Trial {trial} was unable to be loaded.")
+
+    return data, labels
+
+def split_data(data: list[list[float]], labels: list[float], pc_train: float = 0.7) -> tuple[list[list[float]], list[float], list[list[float]], list[float]]:
+    sp_point: int = int(len(data) * pc_train)
+    return data[:sp_point], labels[:sp_point], data[sp_point:], labels[sp_point:]
+
+text: str = read_file("lclean.csv")
+data, labels = load_data(text)
+train_data, train_labels, test_data, test_labels = split_data(data, labels)
+
+clf = MLPRegressor(solver='lbfgs',
+                    alpha=1e-5,
+                    hidden_layer_sizes=(3,2),
+                    random_state=1)
+
+clf.fit(train_data, train_labels)
+
+train_predict = clf.predict(train_data)
+test_predict = clf.predict(test_data)
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.scatter([x[0] for x in test_data], [x[1] for x in test_data], test_predict)
+ax.set_xlabel("is b")
+ax.set_ylabel("is final")
+ax.set_zlabel("pred. reaction_rt")
+plt.show()
